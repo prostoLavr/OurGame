39c7b1bd5bde1d58917b5f8b201e6aa8c1f5e693
Demyan228 <bubnovdema@gmail.com>
2021-04-27 09:18:14 +0800

    add ore

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ import threading
 import os
 import pickle
 from PIL import Image, ImageDraw
+from random import randint
 
 
 WIDTH = 750
@@ -11,12 +12,14 @@ HEIGHT = 500
 FPS = 30
 
 PLAYER_SIZE = (50,) * 2
+ORD_SIZE = (50, ) * 2
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+YELLOW = (225, 225, 0)
 
 FORWARD = 0
 BACKWARD = 1
@@ -27,9 +30,12 @@ PLAYER_SPEED = 10
 
 BACKGROUND_COLOR = (204, 51, 51)
 
+ORD_COLOR = {"diamond": BLUE, "gold": YELLOW}
+
 # Пока что все будет локально, ибо серверов у нас нет.
 addr = ('127.0.0.1', 9090)
 players = []
+ores = []
 
 
 def client(sock, addr, player):
@@ -112,6 +118,54 @@ class MyPlayer(Player):
         # server_sender(('move', self.coord, self))
 
 
+def random_cords():
+    s_x = ORD_SIZE[0] // 2
+    s_y = ORD_SIZE[1] // 2
+    x = randint(s_x, WIDTH - s_x)
+    y = randint(s_y, HEIGHT - s_y)
+    return x, y
+
+class Ore(pygame.sprite.Sprite):
+    def __init__(self, kind, coord=(0, 0)):
+        pygame.sprite.Sprite.__init__(self)
+        self.coord = list(coord)
+        self.id, self.socket = (), None
+        self.create_sprite(kind)
+        self.rect.x = self.coord[0]
+        self.rect.y = self.coord[1]
+
+    def create_sprite(self, kind):
+        self.image = pygame.image.load(OreMaker.make(kind)).convert()
+        self.image.set_colorkey(BACKGROUND_COLOR)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def mined(self):
+        self.coord = list(random_cords())
+        self.rect.x = self.coord[0]
+        self.rect.y = self.coord[1]
+
+    def update(self):
+        pass
+
+
+class OreMaker:
+    @staticmethod
+    def make(kind: str):
+        if not os.path.isdir("res"):
+            os.mkdir("res")
+        if not os.path.isdir("res/images"):
+            os.mkdir("res/images")
+        if kind + "png" in os.listdir(path="res/images"):
+            return f'res/images/{kind}.png'
+        color = ORD_COLOR[kind]
+        img = Image.new('RGB', PLAYER_SIZE, color=BACKGROUND_COLOR)
+        drawer = ImageDraw.Draw(img)
+        drawer.ellipse((0, 0, *PLAYER_SIZE), fill=color)
+        img.save(f'res/images/{kind}.png')
+        return f'res/images/{kind}.png'
+
+
 class CircleMaker:
     @staticmethod
     def make(color, player_number: int):
@@ -134,7 +188,13 @@ class Game:
         self.clock = pygame.time.Clock()
         self.sprites = pygame.sprite.Group()
         self.init_circle()
+        self.init_ore()
         self.game_loop()
+
+    def init_ore(self):
+        ore = Ore("gold", random_cords())
+        ores.append(ore)
+        self.sprites.add(ore)
 
     def init_circle(self):
         self.me = MyPlayer(1, GREEN)
